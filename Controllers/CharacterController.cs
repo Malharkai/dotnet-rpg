cc8fa751d1cac9df3dcf16d2c28b33837749c1c1
Malte <malte.hernandez@iths.se>
2022-09-30 10:50:59 +0200

    #

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotnet_rpg.Services.CharacterService;
 using Microsoft.AspNetCore.Mvc;
 namespace dotnet_rpg.Controllers
 {
@@ -9,12 +10,28 @@ namespace dotnet_rpg.Controllers
     [Route("api/[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static Character knight = new Character();
-
-        [HttpGet]
-        public ActionResult<Character> Get()
+        private readonly ICharacterService _characterService;
+        public CharacterController(ICharacterService service)
         {
-            return Ok(knight);
+            _characterService = service;
+            
+        }
+
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> Get()
+        {
+            return Ok(_characterService.GetAllCharacters());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Character> GetSingle(int id)
+        {
+            return Ok(_characterService.GetCharacterById(id));
+        }
+
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character newCharacter){
+            return Ok(_characterService.AddCharacter(newCharacter));
         }
     }
 }
